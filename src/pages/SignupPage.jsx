aad0abb7da5ead06c4bfc978795b57888edb7572
kaushal-Prakash <147122977+kaushal-Prakash@users.noreply.github.com>
2024-09-15 00:29:05 +0530

    Update SignupPage.jsx

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -27,30 +27,30 @@ function SignupPage() {
 
     return (
         <div className='h-screen flex items-center justify-center p-20 sm:p-10'>
-      <form onSubmit={handleSignup} className="w-full max-w-lg p-12 bg-transparent border-2 border-amber-50 rounded-lg shadow-md glowing-border">
-        <Input
-          label="Your email"
-          type="email"
-          value={email}
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-          id="email"
-          placeholder="Enter Your Email"
-          className="mb-4"
-        />
-        <Input
-          label="Your password"
-          type="password"
-          value={pass}
-          name="password"
-          onChange={(e) => setPass(e.target.value)}
-          id="password"
-          placeholder="Enter Your Password"
-          className="mb-4"
-        />
-        <Button type="submit" text="Submit" />
-      </form>
-    </div>
+            <form onSubmit={handleSignup} className="w-full max-w-lg p-12 bg-transparent border-2 border-amber-50 rounded-lg shadow-md glowing-border sm:w-full">
+                <Input
+                    label="Your email"
+                    type="email"
+                    value={email}
+                    name="email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    id="email"
+                    placeholder="Enter Your Email"
+                    className="mb-4"
+                />
+                <Input
+                    label="Your password"
+                    type="password"
+                    value={pass}
+                    name="password"
+                    onChange={(e) => setPass(e.target.value)}
+                    id="password"
+                    placeholder="Enter Your Password"
+                    className="mb-4"
+                />
+                <Button type="submit" text="Submit" />
+            </form>
+        </div>
     );
 }
 
