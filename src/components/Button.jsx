890cfd71725f09bc41bc21459a0a06e8169823a8
kaushal-Prakash <147122977+kaushal-Prakash@users.noreply.github.com>
2024-09-15 00:09:09 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,7 +7,7 @@ const Button = ({ type, text, className= "" }) => {
         type={type}         
         className={`text-white bg-blue-700 hover:bg-blue-800 
         focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm 
-        w-full sm:w-auto px-5 py-2.5 sm:py-1.2 sm:px-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
+        w-full sm:w-auto px-5 sm:py-2.5 py-1.2 sm:px-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 
         dark:focus:ring-blue-800 ${className} 
         md:w-auto md:px-5 md:py-2.5 md:text-sm 
         sm:w-full sm:px-4 sm:py-2 sm:text-xs`}
