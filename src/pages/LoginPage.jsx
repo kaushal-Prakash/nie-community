4b850c38833fe25b0c7d23be601e0b01449c877a
kaushal-Prakash <147122977+kaushal-Prakash@users.noreply.github.com>
2024-09-15 00:33:34 +0530

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -34,8 +34,8 @@ function LoginPage() {
   };
 
   return (
-    <div className='h-screen flex items-center justify-center p-20 sm:p-10'>
-      <form onSubmit={handleLogin} className="w-full max-w-lg p-12 bg-transparent border-2 border-amber-50 rounded-lg shadow-md glowing-border sm:w-full">
+    <div className='h-screen flex items-center justify-center p-20 sm:w-screen sm:p-10'>
+      <form onSubmit={handleLogin} className="w-full max-w-lg p-12 bg-transparent border-2 border-amber-50 rounded-lg shadow-md glowing-border sm:w-screen">
         <Input
           label="Your email"
           type="email"
